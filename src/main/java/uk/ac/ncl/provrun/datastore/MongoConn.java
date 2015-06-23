aa961556b6be8100f7b0d7fe79b4ec81d68721a6
Simon Bain <smn.bain@gmail.com>
2015-06-23 12:55:40 +0100

    updated .gitignore II

--- src/main/java/uk/ac/ncl/provrun/datastore/MongoConn.java
+++ src/main/java/uk/ac/ncl/provrun/datastore/MongoConn.java
@@ -3,5 +3,6 @@ package uk.ac.ncl.provrun.datastore;
 /**
  * Created by simon on 23/06/15.
  */
-public class MongoConn {
+public class MongoConn implements ProvConn{
+
 }
--- src/main/java/uk/ac/ncl/provrun/datastore/ProvConn.java
+++ src/main/java/uk/ac/ncl/provrun/datastore/ProvConn.java
@@ -3,6 +3,6 @@ package uk.ac.ncl.provrun.datastore;
 /**
  * Created by simon on 23/06/15.
  */
-public class ProvConn {
+public interface ProvConn {
 
 }
